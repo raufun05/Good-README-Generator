d7335310a4016f20541ab08dd4c15a15ebb72e22
Raufun <raufun05@yahoo.com>
2020-08-20 10:41:08 -0600

    added node_modules dependencies

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+var inquirer = require("inquirer");
+const fs = require('fs');
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "What is your location?"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "What is your LinkedIn URL?"
+    }
+  ]).then(function(data) {
+    console.log(data);
+    const boilerHTML =
+      `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+      </head>
+      <body>
+          <ul>
+            <li>${data.name}</li>
+            <li>${data.location}</li>
+            <li>${data.linkedin}</li>
+          </ul>
+      </body>
+      </html>`
+    fs.writeFile("index.html", boilerHTML, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!");
+  });
+});
 // array of questions for user
 const questions = [
 
