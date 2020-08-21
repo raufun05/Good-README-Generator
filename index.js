7d7108b8819165d38b6730727a1bd71721a7443c
Raufun <raufun05@yahoo.com>
2020-08-21 10:55:20 -0600

    added questions to .js file

--- index.js
+++ index.js
@@ -1,60 +1,93 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require('fs');
-inquirer
+const util= require('util');
+const { clear } = require("console");
+
+const writeFileAsync= util.promisify(fs.writeFile);
+const promptUser = () => {
+return inquirer
   .prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?"
+      name: "badge",
+      message: "input badge code?"
     },
     {
       type: "input",
-      name: "location",
-      message: "What is your location?"
+      name: "title",
+      message: "What is your project title?",
+      default: "Generate a good README.md file "
     },
     {
       type: "input",
-      name: "linkedin",
-      message: "What is your LinkedIn URL?"
+      name: "description",
+      message: "Please provide your project description"
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the installation instructions?"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please describe usage"
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Choose a License",
+      choices: [
+        "MIT License",
+        "Apache License",
+        "GPL License",
+        "ISC Licence"]
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What are the rules for contributing?"
+    },
+    {
+      type: "input",
+      name: "author",
+      message: "Who is the author?"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Provide examples on how to run tests"
+    },
+    // {
+    //   type: "input",
+    //   name: "question1",
+    //   message: "Enter the url of your profile picture"
+    // },
+    // // format: ![alt text] (url)
+    // {
+    //   type: "input",
+    //   name: "question2",
+    //   message: "Enter the email address"
+    // },
+    // {
+    //   type: "input",
+    //   name: "question3",
+    //   message: "Enter the url of your github repository"
+    // },
+    {
+      type: "input",
+      name: "table",
+      message: "write a table of contents separated by comas"
     }
-  ]).then(function(data) {
-    console.log(data);
-    const boilerHTML =
-      `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
-      </head>
-      <body>
-          <ul>
-            <li>${data.name}</li>
-            <li>${data.location}</li>
-            <li>${data.linkedin}</li>
-          </ul>
-      </body>
-      </html>`
-    fs.writeFile("index.html", boilerHTML, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("Success!");
-  });
-});
-// array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
+  ]);
+};
+const init = async() => {
+  try {
+    const data= await promptUser();
+    await writeFileAsync("README.md", generateMarkdown(data));
+  }catch(error){
+console.log(error)
+  }
+};
 init();
